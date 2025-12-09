c2540d9cfa1384379c6dcf67273b2178a55daa9c
Robert Varga <varga.robert@gmail.com>
2025-12-09 17:43:51 +0100

    added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,38 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'ProcessSpy Docs',
-            logo: {
-                src: './src/assets/logo.png',
-            },
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/robert-v/ProcessSpy-public' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-                        { label: 'Introduction', slug: 'guides/introduction' },
-						{ label: 'Interface Basics', slug: 'guides/interface_basics' },
-                        { label: 'Table Columns', slug: 'guides/table_columns' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'ProcessSpy Docs',
+          logo: {
+              src: './src/assets/logo.png',
+          },
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/robert-v/ProcessSpy-public' }],
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Introduction', slug: 'guides/introduction' },
+                      { label: 'Interface Basics', slug: 'guides/interface_basics' },
+                      { label: 'Table Columns', slug: 'guides/table_columns' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  adapter: vercel({
+      webAnalytics: {
+          enabled: true,
+      }
+  }),
 });
